1321a16210ad129e6a2052fe5de6c3ceef184e14
Praepanwa <praepanwa.phaeng@mail.kmutt.ac.th>
2024-08-03 17:39:24 +0700

    [UPDATE] prefix file name

--- writefile.py
+++ writefile.py
@@ -4,7 +4,7 @@ import time
 file_name = 'msg_'+str(int(time.time()))
 
 def saveobj(jsonobj):
-    file_name = 'msg_'+str(int(time.time()))
+    file_name = 'chats_'+str(int(time.time()))
     print(file_name)
     with open(f'./data/{file_name}.json', 'w', encoding='utf-8') as f:
         json.dump(jsonobj, f,ensure_ascii=False)
